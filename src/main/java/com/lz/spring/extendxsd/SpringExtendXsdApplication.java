7b5753dbad70d95168fdde5734e374019551f8f6
killper <1060768941@qq.com>
2018-10-24 11:01:53 +0800

    1

---
+++ src/main/java/com/lz/spring/extendxsd/SpringExtendXsdApplication.java
@@ -0,0 +1,15 @@
+package com.lz.spring.extendxsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 10607
+ */
+@SpringBootApplication
+public class SpringExtendXsdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExtendXsdApplication.class, args);
+    }
+}
